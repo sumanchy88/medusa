2a9b438167bf27ea55a11709d9f872cc792066bd
sumanchy88 <sumanchy88@users.noreply.github.com>
2024-08-18 01:02:43 +0000

    chore(docs): Generated API Reference (v2)

--- www/apps/api-reference/specs/admin/code_samples/Shell/admin_orders_{id}_fulfillments_{fulfillment_id}_shipments/post.sh
+++ www/apps/api-reference/specs/admin/code_samples/Shell/admin_orders_{id}_fulfillments_{fulfillment_id}_shipments/post.sh
@@ -4,8 +4,8 @@ curl -X POST '{backend_url}/admin/orders/{id}/fulfillments/{fulfillment_id}/ship
 --data-raw '{
   "items": [
     {
-      "id": "id_uHIzYcTxCHG",
-      "quantity": 3083340957614080
+      "id": "id_FuEHDxWuMi7UC0Zrx",
+      "quantity": 5311246198898688
     }
   ],
   "metadata": {}
